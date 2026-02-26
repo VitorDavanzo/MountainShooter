f552203931a1edd40630556012a81f262a5ff53a
Vitor Davanzo <vitorsdavanzo@gmail.com>
2026-02-26 15:14:49 -0300

    Creating asset folder inserting all assets Creating the Menu: Background, sound and fonts Creating the const file

---
+++ code/const.py
@@ -0,0 +1,15 @@
+#esse arquivo é para definir constantes. no jogo ele será utilizado para localizar melhor essas constantes, no caso, o tamanho da janela. Ao inves de ficar procurando onde está esse atributo, é só alterar a que aparece nesse arquivo
+# #Pela PEP8 (acho que é isso), constantes deve ser representada sem pre em caixa ALTA
+#a cada nova constante, oraganizar em ordem alfabética, comentando a letra
+
+#C
+COLOR_ORANGE = (255,128,0)
+COLOR_YELLOW = (240,240,120)
+COLOR_WHITE = (255,255,255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P','NEW GAME 2P -COOPERATIVE', 'NEW GAME 2P - COMPETITIVE', 'SCORE', 'EXIT')
+
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.const import WIN_WIDTH, COLOR_ORANGE, COLOR_YELLOW, COLOR_WHITE, MENU_OPTION
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load("./asset/menubg.png")
+        self.rect = self.surf.get_rect(left=0, top=0)
 
-    def move(self, ):
-        pass
+    def run(self, ):
+        pygame.mixer.music.load('./asset/menu.mp3')
+        pygame.mixer.music.play(
+            loops=-1)  # quando aparece o parametro laranjinha no nome, 'loops', por exemplo, ele pode ser desconsiderado = poderia ser somente '...play(-1)' -> esse -1 faz com que a musica fique se repetindo
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'Mountain', COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, 'Shooter', COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            # self.menu_text(20, 'NEW GAME 1P', COLOR_YELLOW, ((WIN_WIDTH / 2), 200))
+            # self.menu_text(20, 'NEW GAME 2P - COOPERATIVE', COLOR_WHITE, ((WIN_WIDTH / 2), 225))
+            # self.menu_text(20, 'NEW GAME 2P - COMPETITIVE', COLOR_WHITE, ((WIN_WIDTH / 2), 250))
+            # self.menu_text(20, 'SCORE', COLOR_WHITE, ((WIN_WIDTH / 2), 275))
+            # self.menu_text(20, 'EXIT', COLOR_WHITE, ((WIN_WIDTH / 2), 300))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # End Pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
