a21bdf0198d0db40cd8c84fcef417ff53623ccd4
Vitor Davanzo <vitorsdavanzo@gmail.com>
2026-03-03 09:53:28 -0300

    Inserting all assets in folder asset

--- code/menu.py
+++ code/menu.py
@@ -11,11 +11,11 @@ from code.const import WIN_WIDTH, COLOR_ORANGE, COLOR_YELLOW, COLOR_WHITE, MENU_
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load("./asset/menubg.png")
+        self.surf = pygame.image.load("./asset/MenuBg.png").convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pygame.mixer.music.load('./asset/menu.mp3')
+        pygame.mixer.music.load('./asset/Menu.mp3')
         pygame.mixer.music.play(
             loops=-1)  # quando aparece o parametro laranjinha no nome, 'loops', por exemplo, ele pode ser desconsiderado = poderia ser somente '...play(-1)' -> esse -1 faz com que a musica fique se repetindo
         while True:
